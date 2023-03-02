0013476ab85f857c8f944dd583ecbf97fe38e4e6
František Žiačik <ziacik@gmail.com>
2023-03-02 22:09:54 +0100

    feat: search and show works on home

--- apps/nativescript-amelie-ao3/src/features/home/components/home.component.ts
+++ apps/nativescript-amelie-ao3/src/features/home/components/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Work } from '../../../core/work';
 import { setStatusBarColor } from '../../../utils';
+import { SearchService } from '../../search/search.service';
 
 @Component({
 	moduleId: module.id,
@@ -7,7 +10,14 @@ import { setStatusBarColor } from '../../../utils';
 	templateUrl: './home.component.html',
 })
 export class HomeComponent {
+	works?: Observable<Work[]>;
+
+	constructor(private readonly searchService: SearchService) {}
+
 	ngOnInit() {
 		setStatusBarColor('dark', '#97d9e9');
+		this.works = this.searchService.search(
+			'Wednesday Addams Is Soft For Enid Sinclair'
+		);
 	}
 }
